403da8f04e0d0c415dc59a8985cc7b075384d5c6
Michał Chodkiewicz <michal.chodkiewicz@gmail.com>
2025-10-21 09:46:23 +0200

    initial commit

---
+++ include/test_something/test_math.h
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace test_math{
+
+int add(int a, int b);
+int multiply(int a, int b);
+
+}
+
---
+++ src/test_math.cpp
@@ -0,0 +1,17 @@
+#include "test_something/test_math.h"
+
+namespace test_math{
+
+int add(int a, int b)
+{
+    return a+b;
+}
+
+int multiply(int a, int b)
+{
+    return a*b;
+}
+
+}
+
+
